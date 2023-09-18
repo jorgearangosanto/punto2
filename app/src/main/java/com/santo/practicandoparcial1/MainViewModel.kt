7d98997f5ed1b5a2a703fc957d45ce0a17726d77
santo <jorge.arangoa@udea.edu.co>
2023-09-17 23:05:06 -0500

    guardando feature practica2

--- app/src/main/java/com/santo/practicandoparcial1/MainViewModel.kt
+++ app/src/main/java/com/santo/practicandoparcial1/MainViewModel.kt
@@ -1,15 +1,41 @@
 package com.santo.practicandoparcial1
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel(){
+
+
+    val contrasena: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val mensajeerror: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val info: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
     fun validatepasswor(password: String, repasword: String) {
         if (password != repasword){
+            mensajeerror.value=" las contrasenas no son iguale"
 
     }else {
-           val password = repasword
+            contrasena.value = password.toString()
     }
 
+
+
+   /* fun registro(email: String, password: String) {
+         info.value = email.toString() + password.toString()
+
+    }*/
+
     }
+
+    fun registro(email: String, password: String) {
+        info.value = email.toString() + password.toString()
+    }
+
 
 }
--- app/src/main/java/com/santo/practicandoparcial1/RegisterActivity2.kt
+++ app/src/main/java/com/santo/practicandoparcial1/RegisterActivity2.kt
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.santo.practicandoparcial1.databinding.ActivityRegister2Binding
 import java.text.SimpleDateFormat
 import java.util.*
+import androidx.lifecycle.Observer
 
 class RegisterActivity2 : AppCompatActivity() {
 
     private lateinit var register2Binding: ActivityRegister2Binding
-    private lateinit var mainviewModel: MainViewModel
+    private lateinit var registerActivity2: MainViewModel
     private var fechaNacimiento: String = ""
     private var cal= Calendar.getInstance()
     private var estado =""
@@ -20,8 +22,24 @@ class RegisterActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         register2Binding= ActivityRegister2Binding.inflate(layoutInflater)
+        registerActivity2 = ViewModelProvider(this)[MainViewModel::class.java]
        val view = register2Binding.root
         setContentView(view)
+
+
+
+        val contrasenaobserver = Observer<String>{contrasena->
+            register2Binding.infoTextView.setText(contrasena.toString())
+
+        }
+        registerActivity2.contrasena.observe(this,contrasenaobserver)
+
+        val mensajeerrorobserver = Observer<String>{mensajeerror->
+            register2Binding.infoTextView.setText(mensajeerror.toString())
+
+        }
+        registerActivity2.mensajeerror.observe(this,mensajeerrorobserver)
+
         val dataSetListener =
             DatePickerDialog.OnDateSetListener { view, year: Int, month, DayOfMonth ->
                 cal.set(Calendar.YEAR,year)
@@ -47,13 +65,15 @@ class RegisterActivity2 : AppCompatActivity() {
         }
 
         register2Binding.registerButton.setOnClickListener {
-            Log.d( "button" , "clicked")
+            registerActivity2.registro(register2Binding.emailEditText.text.toString(),register2Binding.passworeditex.text.toString()
+            )
+          /*  Log.d( "button" , "clicked")
             val email = register2Binding.emailEditText.text.toString()
             register2Binding.infoTextView.text = email
 
             val password = register2Binding.passworeditex.text.toString()
 
-            val repasword = register2Binding.repPassworEditText.text.toString()
+            val repasword = register2Binding.repPassworEditText.text.toString()*/
 
             var genre = "masculino"
             if ( register2Binding.femalesRadioButton.isChecked)
@@ -73,7 +93,7 @@ class RegisterActivity2 : AppCompatActivity() {
             if (register2Binding.fictionCheckBox.isChecked)
                 genrefavorite = genrefavorite + "  " + "ficcion"
 
-            mainviewModel.validatepasswor(register2Binding.passworeditex.text.toString(),register2Binding.repPassworEditText.text.toString())
+            registerActivity2.validatepasswor(register2Binding.passworeditex.text.toString(),register2Binding.repPassworEditText.text.toString())
 
             /*if (password == repasword){
                val  info =email +"\n" + password + "\n" +genre+ "\n" + genrefavorite+ "\n" +fechaNacimiento
